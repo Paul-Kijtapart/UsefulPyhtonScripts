fd9a08dc7eaa740bdd8182f572a9ad63c4ae2e0b
paulnawat <scc385@gmail.com>
2018-05-14 23:50:54 -0700

    feat: implement sort list of color codes

--- algorithm/sorts.py
+++ algorithm/sorts.py
@@ -1,7 +1,7 @@
 class Sorts:
 
     @staticmethod
-    def sort_anagram(words:list) -> list:
+    def sort_anagram(words: list) -> list:
         """
 
         Args:
@@ -25,7 +25,7 @@ class Sorts:
 
             key = "".join(sorted(word))
 
-            # update map with current word
+            # update map with base_color word
             if key in anagram_map:
                 anagram_map[key].append(word)
             else:
@@ -38,3 +38,35 @@ class Sorts:
             res += anagram_map[key]
 
         return res
+
+    @staticmethod
+    def sort_colors(colors):
+        """
+        sorted the given colors in-place
+        the same color are adjacent, with the colors in the order red, white and blue.
+        we will use the integers 0, 1, and 2 to represent the color red, white, and blue respectively.
+
+        Args:
+            colors(List[int]): list of colors codes
+                               0 = red,
+                               1 = white
+                               2 = blue
+
+        Returns:
+
+        """
+        i = 1
+
+        while i < len(colors):
+
+            j = i - 1
+
+            base_color = colors[i]
+
+            while j >= 0 and colors[j] > base_color:
+                colors[j + 1] = colors[j]
+                j -= 1
+
+            colors[j + 1] = base_color
+
+            i += 1
