a63396750fd075c3d60d8afd14d00a91d6b594da
paulnawat <scc385@gmail.com>
2018-05-15 23:55:30 -0700

    chore: add more tests for the find_diff_s_t

--- tests/algorithm/test_strings.py
+++ tests/algorithm/test_strings.py
@@ -6,5 +6,7 @@ from algorithm.strings import Strings
 class TestStrings(TestCase):
     def test_find_the_difference(self):
         # s = "abcd", t = "abcde" => 'e'
-
         self.assertEqual(Strings.find_the_difference('abcd', 'abcde'), 'e')
+
+        # s = "abcd", t = "abcde" => 'e'
+        self.assertEqual(Strings.find_the_difference_pro('abcd', 'abcde'), 'e')
