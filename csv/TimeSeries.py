ae52d4cf463b4a83ce1335c58107854a34558756
paulnawat <scc385@gmail.com>
2018-05-23 22:06:01 -0700

    feat: add template to generate mock time series csv data

---
+++ csv/TimeSeries.py
@@ -0,0 +1,20 @@
+# from pick import pick
+
+
+class TimeSeries:
+
+    @staticmethod
+    def generate_random_time_series():
+        # resolution = input("Please select resolution")
+        # timeframe = input("Please select time frame")
+
+        # add Date column with timestamp (space = resolution)
+
+        # Note: one variable = one column with random values for each timestamp
+        # num_variables = input("Please enter the number of variables to generate")
+
+        # title = 'Please choose your favorite programming language: '
+        # options = ['Java', 'JavaScript', 'Python', 'PHP', 'C++', 'Erlang', 'Haskell']
+        # option, index = pick(options, title)
+        # print(option)
+        print('gg')
